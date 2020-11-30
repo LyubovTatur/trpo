7b935cd2b9e60e77c88c423fddeb63ee4a2862cb
LyubovTatur <lubatatur58@gmail.com>
2020-11-30 16:20:25 +0300

    done!

---
+++ refl/delegats/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        delegate int Operation(int x, int y);
+        delegate T myDrlrgate<T>(int val1, string val2, double val3, T myval);
+        static T MyDelFunc<T>(int val1, string val2, double val3, T myval)
+        {
+            Console.WriteLine("вызвана MyDelFunc");
+            return myval;
+        }
+        static string MyFunc<T>(T deleg)
+        {
+            return "вызвана MyFunc";
+        }
+
+        static void Main(string[] args)
+        {
+            myDrlrgate<int> op = MyDelFunc; 
+            Console.WriteLine(  MyFunc<int>(op(3, "gfd", 4.2, 5)));         //1
+            Operation operation = (x, y) => x + y;
+            Console.WriteLine(operation(10, 20));                           //2
+            Console.WriteLine(MyFunc<int>(operation(23, 55)));              //3
+            Func<int, string, double, int, int> f = op.Invoke;
+            Console.WriteLine(MyFunc<int>(f(3, "gfd", 4.2, 5)));            //4
+            Console.ReadLine();
+
+        }
+
+
+
+
+    }
+}
---
+++ refl/refl/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myType = Type.GetType("ConsoleApp1.MyClass", false, true);
+            foreach (MemberInfo mi in myType.GetMembers())
+            {
+                Console.WriteLine($"{mi.DeclaringType} {mi.MemberType} {mi.Name}");
+
+            }
+            foreach (ConstructorInfo ctor in myType.GetConstructors())
+            {
+                Console.Write(myType.Name + " (");
+                // получаем параметры конструктора
+                ParameterInfo[] parameters = ctor.GetParameters();
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    Console.Write(parameters[i].ParameterType.Name + " " + parameters[i].Name);
+                    if (i + 1 < parameters.Length) Console.Write(", ");
+                }
+                Console.WriteLine(")");
+            }
+            Console.WriteLine("Поля:");
+            foreach (FieldInfo field in myType.GetFields())
+            {
+                Console.WriteLine($"{field.FieldType} {field.Name}");
+            }
+
+            Console.WriteLine("Свойства:");
+            foreach (PropertyInfo prop in myType.GetProperties())
+            {
+                Console.WriteLine($"{prop.PropertyType} {prop.Name}");
+            }
+            User bob = new User(16);
+            User tom = new User(34);
+            bool bobIsValid = ValidateUser(bob);    
+            bool tomIsValid = ValidateUser(tom);    
+
+            Console.WriteLine($"Реультат валидации Боба: {bobIsValid}");
+            Console.WriteLine($"Реультат валидации Тома: {tomIsValid}");
+
+            
+            Type myType2 = Type.GetType("ConsoleApp1.User", false, true);
+            Console.WriteLine(myType2.GetMethod("SayHi").Invoke(null, null));
+            Console.ReadLine();
+        }
+        static bool ValidateUser(User user)
+        {
+            Type t = typeof(User);
+            object[] attrs = t.GetCustomAttributes(false);
+            foreach (AtClass attr in attrs)
+            {
+                if (user.Age >= attr.Age) return true;
+                else return false;
+            }
+            return true;
+        }
+    }
+
+    class MyClass
+    {
+        public int i;
+        public int Age { get; set; }
+        public MyClass(int i)
+        {
+            this.i = i;
+        }
+        public int GetI()
+        {
+            return i;
+        }
+
+
+    }
+    [AtClass(Age  = 18)]
+    public class User
+    {
+
+        public int Age { get; set; }
+        public User(int a)
+        {
+
+            Age = a;
+        }
+        public static void SayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+    class AtClass : System.Attribute
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public AtClass()
+        { }
+
+        
+    }
+
+}
